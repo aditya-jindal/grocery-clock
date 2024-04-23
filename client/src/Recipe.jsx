742fda6d89e09185a1b4c3532fc1d7df3410c631
aditya-jindal <adityajindal1509@gmail.com>
2024-04-23 18:36:31 +0530

    fixed a bug to handle null api responses

--- client/src/Recipe.jsx
+++ client/src/Recipe.jsx
@@ -6,15 +6,19 @@ export function Recipie({ recipie, setShowRecipie, recipies }) {
       }
       className="flex border-2 shadow-md bg-white mx-3 rounded-xl justify-between cursor-pointer"
     >
-      <img
-        src={recipie.image}
-        alt={recipie.name}
-        className="object-contain w-1/6 rounded-l-xl ml-0"
-      />
-      <div className="flex flex-col gap-y-2 w-5/6 px-6 py-2">
-        <h1 className="text-2xl font-semibold">{recipie.name}</h1>
-        <p>Preparation Time: {recipie.prepTime} mins</p>
-      </div>
+      {recipie.image && (
+        <>
+          <img
+            src={recipie.image}
+            alt={recipie.name}
+            className="object-contain w-1/6 rounded-l-xl ml-0"
+          />
+          <div className="flex flex-col gap-y-2 w-5/6 px-6 py-2">
+            <h1 className="text-2xl font-semibold">{recipie.name}</h1>
+            <p>Preparation Time: {recipie.prepTime} mins</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
